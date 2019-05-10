74ae182914d5a122afa45b8030a8c50f9015ee81
Mark Moeller <MarkMoellerUVA@gmail.com>
2019-05-10 15:35:32 -0400

    animation for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react'
 import './Footer.css'
 
 export default class Footer extends Component {
+  state = {
+    anim: null
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ anim: 'anim' })
+    }, 1000)
+  }
+
   render() {
-    return <div className="copyright">© 2016 Copyright brstso</div>
+    return (
+      <div className={`copyright ${this.state.anim}`}>
+        © 2016 Copyright brstso
+      </div>
+    )
   }
 }
