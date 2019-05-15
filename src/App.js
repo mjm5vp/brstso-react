8f315a5f77144e6729bb85e4bfeddf8ca0512042
Mark Moeller <MarkMoellerUVA@gmail.com>
2019-05-15 11:32:39 -0400

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import Home from './pages/Home/Home'
 import CompanyInfo from './pages/CompanyInfo/CompanyInfo'
