1e121e1478ed63f2ab868fc05a070bd944476dbc
Mark Moeller <MarkMoellerUVA@gmail.com>
2019-05-23 15:32:11 -0400

    added build and deploy script

---
+++ brsto-build/precache-manifest.cfc880cf2b7069ff30faa6a97ff3e5a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236c4246fe1aac22ca30f1400ef54f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef524b1e6f71e69124a6",
+    "url": "/static/css/main.03b065ce.chunk.css"
+  },
+  {
+    "revision": "d07b2ca9d8a1d9df0de9",
+    "url": "/static/js/2.31dc1f8f.chunk.js"
+  },
+  {
+    "revision": "ef524b1e6f71e69124a6",
+    "url": "/static/js/main.032e1e8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb54cfabcc6dd9875d76bfdbe471a5b2",
+    "url": "/static/media/email.eb54cfab.png"
+  },
+  {
+    "revision": "11e0d4f23aa7d7e662f5dc041faf34e9",
+    "url": "/static/media/logo.11e0d4f2.png"
+  },
+  {
+    "revision": "3e8b26fc9e464e163acefb16b90130bd",
+    "url": "/static/media/martin-lucero-headshot.3e8b26fc.png"
+  },
+  {
+    "revision": "e548d8b5ab30e10071860b5799a1fc85",
+    "url": "/static/media/small-logo.e548d8b5.png"
+  }
+]);
